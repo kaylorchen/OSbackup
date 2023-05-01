b27494ab7718d9ba9197aa4c5c8d0e82e0550b4e
kaylor <kaylor.chen@qq.com>
2023-05-01 14:13:23 +0800

    Update

--- extended_partition.sh
+++ extended_partition.sh
@@ -6,18 +6,19 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 dev=${1}
+partition=${2}
 parted -s ${dev} p
-parted -s ${dev} resizepart 2 100%
+parted -s ${dev} resizepart ${partition} 100%
 parted -s ${dev} p
 
 echo "Associating loopback device to image"
 sudo losetup /dev/loop100 --partscan --show ${dev}
 
 echo "e2fsck is running"
-e2fsck -f /dev/loop100p2
+e2fsck -f /dev/loop100p${partition}
 
 echo "resize2fs is running"
-resize2fs -f /dev/loop100p2
+resize2fs -f /dev/loop100p${partition}
 
 echo "Umounting loopback device"
 sudo losetup -d /dev/loop100
